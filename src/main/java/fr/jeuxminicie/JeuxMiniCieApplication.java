cf0a482dfdcd6ff89eda58bd1cd2c48671a314eb
LionelLou <lourenco.lionel@hotmail.fr>
2022-12-26 17:58:42 +0100

    Initial commit

---
+++ src/main/java/fr/jeuxminicie/JeuxMiniCieApplication.java
@@ -0,0 +1,13 @@
+package fr.jeuxminicie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuxMiniCieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeuxMiniCieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/jeuxminicie/JeuxMiniCieApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jeuxminicie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeuxMiniCieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
