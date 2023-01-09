d6e1c8d281844436a97a2ea6133bc326fc053f0e
LionelLou <lourenco.lionel@hotmail.fr>
2023-01-09 22:39:02 +0100

    finished user controller

--- src/main/java/fr/jeuxminicie/controllers/UserController.java
+++ src/main/java/fr/jeuxminicie/controllers/UserController.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,6 +75,49 @@ public class UserController {
 		}
 		
 	}
+	
+	
+	
+	@PutMapping(value="/update/email", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Object> updateEmail(@RequestBody UserUpdateDto user) throws Exception {
+		
+		try {
+			UserDto result = service.updateUserEmail(user);
+			return ResponseEntity.status(HttpStatus.OK).body(result);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+		
+	}
+	
+	
+	
+	@PutMapping(value="/update/credentials", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Object> updateCredentials(@RequestBody UserUpdateDto user) throws Exception {
+		
+		try {
+			UserDto result = service.updateUserPassword(user);
+			return ResponseEntity.status(HttpStatus.OK).body(result);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+		
+	}
+	
+	
+	@PutMapping(value="/update/email", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Object> resetCredentials(@RequestBody UserUpdateDto user) throws Exception {
+		
+		try {
+			UserDto result = service.resetUserPassword(user);
+			return ResponseEntity.status(HttpStatus.OK).body(result);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+		
+	}
+	
+	
 	
 	
 }
