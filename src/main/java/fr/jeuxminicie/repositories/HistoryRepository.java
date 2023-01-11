bd204819a4c53924f8167ea0d1c1f13db4aba6cd
LionelLou <lourenco.lionel@hotmail.fr>
2023-01-11 03:30:44 +0100

    created base repository for history

--- src/main/java/fr/jeuxminicie/repositories/HistoryRepository.java
+++ src/main/java/fr/jeuxminicie/repositories/HistoryRepository.java
@@ -1,5 +1,9 @@
 package fr.jeuxminicie.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,10 @@ import fr.jeuxminicie.entities.History;
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Long>{
 
+	List<History> findAllByUserNameContainingOrUserEmailContainingOrGameNameContaining(String userName, String userEmail, String gameName);
+
+	Page<History> findAllByUserNameContainingOrUserEmailContainingOrGameNameContaining(String userName, String userEmail, String gameName, Pageable pageable);
+	
+	List<History> findAllByUserId(long userId);
+	
 }
